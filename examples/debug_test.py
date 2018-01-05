a97a7350acc681effade29b98d5deecc3596a125
silviaAmAm <walfits@gmail.com>
2018-01-05 13:30:52 +0000

    Example that checks whether the parameters before and after cloning are the same

--- examples/debug_test.py
+++ examples/debug_test.py
@@ -7,13 +7,17 @@ import tensorflow as tf
 
 estimator = wrappers.OSPMRMP(batch_size = 100, representation = "unsorted_coulomb_matrix",
                     hl1=12, optimiser=tf.train.AdadeltaOptimizer, scoring_function='mae',
-                    iterations=6000, slatm_sigma1 = 0.1)
+                    iterations=6000, slatm_sigma1 = 0.1, activation_function=tf.nn.relu)
 
 # Printing the number of iterations
 print("\n The number of iterations after loading is")
 print(estimator.iterations)
 print("\n The slatm after loading is")
 print(estimator.slatm_sigma1)
+print("\n The optimiser before cloning is:")
+print(estimator.optimiser)
+print("\n The activation function before cloning is:")
+print(estimator.activation_function)
 
 estimator.get_params()
 
@@ -22,5 +26,8 @@ print("\n The slatm after cloning is:")
 print(dolly.slatm_sigma1)
 print("\n The iterations after cloning are:")
 print(dolly.iterations)
-
+print("\n The optimiser after cloning is:")
+print(dolly.optimiser)
+print("\n The activation function after cloning is:")
+print(dolly.activation_function)
 
