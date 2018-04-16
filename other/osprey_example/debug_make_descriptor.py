7afea7d071d40e36da10db7bfb478bcb6173b0ee
Silvia Amabilino <walfits@gmail.com>
2018-04-16 12:59:24 +0100

    small change before switching branch

--- other/osprey_example/debug_make_descriptor.py
+++ other/osprey_example/debug_make_descriptor.py
@@ -5,13 +5,16 @@ from sklearn import model_selection as modsel
 from sklearn.base import clone
 
 estimator = wrappers.OAMNN(iterations=400, batch_size=70, scoring_function="mae", tensorboard=True, l2_reg=0.03906198710720926,
-                        hidden_layer_sizes=[50, 20], learning_rate=0.005, representation="slatm", slatm_dgrid1 = 0.6)
+                        hidden_layer_sizes=[50, 20], learning_rate=0.005, representation="slatm", slatm_dgrid1 = 0.12)
 
 # filenames = glob.glob("/Volumes/Transcend/repositories/Aglaia/data/qm7/*.xyz")[:100]
-filenames = glob.glob("/Volumes/Transcend/data_sets/vr_ccsd/*.xyz")[:100]
+# filenames = glob.glob("/Volumes/Transcend/data_sets/vr_ccsd/*.xyz")[:100]
+filenames = glob.glob("/Volumes/Transcend/data_sets/OH_squalane_model/geoms/training/*.xyz")[:100]
 filenames.sort()
 # energies = np.loadtxt('/Volumes/Transcend/repositories/Aglaia/data/qm7/hof_qm7.txt', usecols=[1])[:100]
-energies = np.loadtxt('/Volumes/Transcend/data_sets/vr_ccsd/properties.txt', usecols=[1])[:100]
+# energies = np.loadtxt('/Volumes/Transcend/data_sets/vr_ccsd/properties.txt', usecols=[1])[:100]
+energies = np.loadtxt('/Volumes/Transcend/data_sets/OH_squalane_model/geoms/training/properties_kjmol.txt', usecols=[1])[:100]
+
 estimator.generate_compounds(filenames)
 estimator.set_properties(energies)
 estimator.generate_descriptor()
@@ -22,17 +25,19 @@ idx_train, idx_test = modsel.train_test_split(idx, test_size=0.3)
 
 train_scores = []
 
-for i in range(3):
+# for i in range(3):
+#
+#     an_estimator = clone(estimator)
+#
+#     an_estimator.fit(idx_train)
+#
+#     score_train = an_estimator.score(idx_train)
+#
+#     train_scores.append(score_train)
 
-    an_estimator = clone(estimator)
-
-    an_estimator.fit(idx_train)
-
-    score_train = an_estimator.score(idx_train)
-
-    train_scores.append(score_train)
-
-
+estimator.fit(idx_train)
+y_pred = estimator.predict(idx_train)
+estimator.correlation_plot(np.squeeze(y_pred), energies[idx_train])
 
 
 # estimator.plot_cost()
