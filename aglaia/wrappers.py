40249cd7990d9c24499ce75e24797513ea4b11da
Silvia Amabilino <walfits@gmail.com>
2018-04-20 11:53:07 +0100

    Modifications to the wrapper for the ARMP class

--- aglaia/wrappers.py
+++ aglaia/wrappers.py
@@ -996,24 +996,27 @@ class OAMNN(ARMP, _ONN):
             zs_tf = tf.placeholder(shape=[n_samples, max_n_atoms], dtype=tf.int32, name="zs")
             xyz_tf = tf.placeholder(shape=[n_samples, max_n_atoms, 3], dtype=tf.float32, name="xyz")
 
-        # descriptor = sf.generate_parkhill_acsf(xyzs=xyz_tf, Zs=zs_tf, elements=elements, element_pairs=element_pairs,
-        #                                        radial_cutoff=self.radial_cutoff, angular_cutoff=self.angular_cutoff,
-        #                                        radial_rs=self.radial_rs, angular_rs=self.angular_rs, theta_s=self.theta_s,
-        #                                        eta=self.eta, zeta=self.zeta)
-
-        # # Uncomment to use the Parkhill implementation of ACSF
-        descriptor = tm_sf.tensormol_acsf(xyz_tf, zs_tf, elements=elements, element_pairs=element_pairs,
+        descriptor = sf.generate_parkhill_acsf(xyzs=xyz_tf, Zs=zs_tf, elements=elements, element_pairs=element_pairs,
                                                radial_cutoff=self.radial_cutoff, angular_cutoff=self.angular_cutoff,
                                                radial_rs=self.radial_rs, angular_rs=self.angular_rs, theta_s=self.theta_s,
                                                eta=self.eta, zeta=self.zeta)
 
+        # Uncomment to use the Parkhill implementation of ACSF
+        # descriptor = tm_sf.tensormol_acsf(xyz_tf, zs_tf, elements=elements, element_pairs=element_pairs,
+        #                                        radial_cutoff=self.radial_cutoff, angular_cutoff=self.angular_cutoff,
+        #                                        radial_rs=self.radial_rs, angular_rs=self.angular_rs, theta_s=self.theta_s,
+        #                                        eta=self.eta, zeta=self.zeta)
+
         sess = tf.Session()
         sess.run(tf.global_variables_initializer())
-        # descriptor_np = sess.run(descriptor, feed_dict={xyz_tf: xyzs, zs_tf: zs})
-        # # Uncomment to get memory and compute time in tensorboard
-        descriptor_np = sess.run(descriptor, feed_dict={xyz_tf: xyzs, zs_tf: zs}, options=options, run_metadata=run_metadata)
-        summary_writer = tf.summary.FileWriter(logdir="tensorboard", graph=sess.graph)
-        summary_writer.add_run_metadata(run_metadata=run_metadata, tag="Descriptor", global_step=None)
+        if self.tensorboard:
+            descriptor_np = sess.run(descriptor, feed_dict={xyz_tf: xyzs, zs_tf: zs}, options=options,
+                                     run_metadata=run_metadata)
+            summary_writer = tf.summary.FileWriter(logdir="tensorboard", graph=sess.graph)
+            summary_writer.add_run_metadata(run_metadata=run_metadata, tag="Descriptor", global_step=None)
+        else:
+            descriptor_np = sess.run(descriptor, feed_dict={xyz_tf: xyzs, zs_tf: zs})
+
         sess.close()
 
         return descriptor_np, zs
