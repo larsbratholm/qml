b5b27afe67a4c05e31747074f672fdc4dc477c6d
Silvia Amabilino <walfits@gmail.com>
2018-07-19 16:55:29 +0100

    Modified ARMP_G and added a data set to test it

--- examples/ARMP_G_1.py
+++ examples/ARMP_G_1.py
@@ -1,7 +1,8 @@
 """
-This script shows how to set up the ARMP estimator where the XYZ data is used to make QML compounds and the local
-descriptors are generated from the QML compounds and then stored.
+This script shows how to set up the ARMP_G estimator where the xyz, nuclear charges, energies and forces are all set in
+advance and are then used for fitting using the indices.
 """
+
 from qml.aglaia.aglaia import ARMP_G
 import glob
 import numpy as np
@@ -9,7 +10,8 @@ import os
 
 ## ------------- ** Loading the data ** ---------------
 
-data = np.load("/Volumes/Transcend/repositories/Aglaia/data/CN_isopentane_forces.npz")
+current_dir = os.path.dirname(os.path.realpath(__file__))
+data = np.load(current_dir+"/../test/data/CN_isopentane_forces.npz")
 
 xyz = data["arr_0"]
 zs = data["arr_1"]
@@ -20,28 +22,24 @@ print(xyz.shape, zs.shape, ene.shape, forces.shape)
 
 ## ------------- ** Setting up the estimator ** ---------------
 
-estimator = ARMP_G(iterations=100, representation='acsf', descriptor_params={"radial_rs": np.arange(0,10, 0.5), "angular_rs": np.arange(0.5, 10.5, 0.5),
-"theta_s": np.arange(0, 3.14, 0.2)}, tensorboard=True, batch_size=10)
+estimator = ARMP_G(iterations=1, representation='acsf', descriptor_params={"radial_rs": np.arange(0,10, 2), "angular_rs": np.arange(0.5, 10.5, 2),
+"theta_s": np.arange(0, 3.14, 2.5)}, tensorboard=False, store_frequency=1, batch_size=10)
 
 estimator.set_xyz(xyz)
 estimator.set_classes(zs)
 estimator.set_properties(ene)
 estimator.set_gradients(forces)
 
-
-
-
 ##  ------------- ** Fitting to the data ** ---------------
 
-idx = np.arange(0,100)
+idx = np.arange(0,5)
 
 estimator.fit(idx)
 
-
 # ##  ------------- ** Predicting and scoring ** ---------------
-#
-# score = estimator.score(idx)
-#
-# print("The mean absolute error is %s kJ/mol." % (str(-score)))
-#
-# energies_predict = estimator.predict(idx)
+
+score = estimator.score(idx)
+
+print("The mean absolute error is %s (kcal/mol)." % (str(-score)))
+
+energies_predict = estimator.predict(idx)
