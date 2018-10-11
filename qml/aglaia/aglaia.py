becb880a9226bc346592df5fe99dd85d557932e7
Silvia Amabilino <walfits@gmail.com>
2018-10-11 14:00:18 +0100

    Added the constant so that one can change the weight of the forces in the cost function.
    Also fixed some bugs

--- qml/aglaia/aglaia.py
+++ qml/aglaia/aglaia.py
@@ -2786,7 +2786,8 @@ class ARMP_G(ARMP, _NN):
                  epsilon=1e-08,
                  rho=0.95, initial_accumulator_value=0.1, initial_gradient_squared_accumulator_value=0.1,
                  l1_regularization_strength=0.0, l2_regularization_strength=0.0,
-                 tensorboard_subdir=os.getcwd() + '/tensorboard', representation_name='acsf', representation_params=None):
+                 tensorboard_subdir=os.getcwd() + '/tensorboard', representation_name='acsf', representation_params=None,
+                 phi=1.0):
 
         super(ARMP_G, self).__init__(hidden_layer_sizes, l1_reg, l2_reg, batch_size, learning_rate,
                                    iterations, tensorboard, store_frequency, tf_dtype, scoring_function,
@@ -2798,6 +2799,22 @@ class ARMP_G(ARMP, _NN):
             raise InputError("Only the acsf representation can currently be used with gradients.")
 
         self._initialise_representation(representation_name, representation_params)
+        self._set_phi(phi)
+
+    def _set_phi(self, phi):
+        """
+        Sets the parameter that weights the forces term in the cost function. If phi = 0, then it will be equivalent to
+        doing the calculation with ARMP, except much slower since the gradients are still calculated.
+
+        :param phi: parameter that multiplies the forces in the cost function
+        :type phi: positive float
+        :return: None
+        """
+
+        if is_positive_or_zero(phi):
+            self.phi = phi
+        else:
+            raise InputError("Phi should be positive or zero.")
 
     def _check_inputs(self, x, y, classes, dy, dgdr):
         """
@@ -2830,9 +2847,8 @@ class ARMP_G(ARMP, _NN):
 
             if is_none(self.xyz) or is_none(self.classes):
                 if not is_none(self.compounds):
-                    idx_tot = len(self.compounds)
-                    self.xyz = self._get_xyz_from_compounds(idx_tot)
-                    self.classes = self._get_classes_from_compounds(idx_tot)
+                    self.xyz = self._get_xyz_from_compounds(x)
+                    self.classes = self._get_classes_from_compounds(x)
                     approved_xyz = self.xyz[x]
                     approved_classes = self.classes[x]
                 else:
@@ -2980,7 +2996,7 @@ class ARMP_G(ARMP, _NN):
 
         return element_weights, element_biases
 
-    def _cost_G(self, y_true, y_nn, dy_true, dy_nn, weights_dict, phi):
+    def _cost_G(self, y_true, y_nn, dy_true, dy_nn, weights_dict):
         """
         This function calculates the cost for the ARMP_G class. It uses both true energies/forces and the neural network
         predicted energies/forces.
@@ -2994,14 +3010,12 @@ class ARMP_G(ARMP, _NN):
         :param dy_nn: Neural network predicted gradients
         :type dy_nn: tf tensor of shape (n_sample, n_atoms, 3)
         :param weights_dict: dictionary containing the weights for each element specific network.
-        :param phi: parameter to weight the forces in the cost function
-        :type phi: float
         :return: tf.tensor of shape ()
         """
 
         ene_err = tf.square(tf.subtract(y_true, y_nn))
         force_err = tf.square(tf.subtract(dy_true, dy_nn))
-        phi_tf = tf.constant(phi, dtype=tf.float32)
+        phi_tf = tf.constant(self.phi, dtype=tf.float32)
 
         cost_function = tf.add(tf.reduce_mean(ene_err), tf.reduce_mean(force_err)*phi_tf, name="loss")
 
@@ -3177,7 +3191,7 @@ class ARMP_G(ARMP, _NN):
 
         # Calculating the cost
         with tf.name_scope("Cost"):
-            cost = self._cost_G(batch_y, energies_nn, batch_dy, forces_nn, element_weights, phi=2)
+            cost = self._cost_G(batch_y, energies_nn, batch_dy, forces_nn, element_weights)
 
         if self.tensorboard:
             cost_summary = self.tensorboard_logger_training.write_cost_summary(cost)
--- test/test_armp_g.py
+++ test/test_armp_g.py
@@ -30,6 +30,8 @@ from qml.utils.utils import InputError
 import glob
 from qml.utils.utils import is_array_like
 import os
+import shutil
+import tensorflow as tf
 
 def test_set_representation():
     """
@@ -84,38 +86,7 @@ def test_set_properties():
 
     assert np.all(estimator.properties == energies)
 
-def test_set_representation():
-    """
-    This test checks that the set_representation function works as expected.
-    :return:
-    """
-    test_dir = os.path.dirname(os.path.realpath(__file__))
-
-    data_incorrect = np.load(test_dir + "/data/CN_isopent_light_UCM.npz")
-    data_correct = np.load(test_dir + "/data/local_slatm_ch4cn_light.npz")
-    representation_correct = np.asarray(data_correct["arr_0"])
-    representation_incorrect = np.asarray(data_incorrect["arr_0"])
-
-    dgdr_correct = np.ones((3, 4, 5, 4, 3))
-    dgdr_incorrect = np.ones((3, 1, 2, 3, 4))
-
-
-    estimator = ARMP_G()
-
-    assert estimator.g == None
-
-    estimator._set_representation(g=representation_correct)
-
-    assert np.all(estimator.g == representation_correct)
-
-    # Pass a representation with the wrong shape
-    try:
-        estimator._set_representation(g=representation_incorrect)
-        raise Exception
-    except InputError:
-        pass
-
-def test_fit_1():
+def test_fit():
     """
     This function tests the first way of fitting the representation: the data is passed by first creating compounds and then
     the representations are created from the compounds.
@@ -127,104 +98,140 @@ def test_fit_1():
                           usecols=[1])
     data = np.load(test_dir + "/data/CN_isopentane_forces.npz")
     filenames.sort()
-    forces =  data["arr_3"][:2]
+    forces =  data["arr_3"][:5]
 
-    estimator = ARMP_G(representation_name="acsf")
-    estimator.generate_compounds(filenames[:2])
-    estimator.set_properties(energies[:2])
-    estimator.set_gradients(forces)
-    estimator.generate_representation()
-
-    idx = np.arange(0, 2)
-    estimator.fit(idx)
-
-def test_fit_2():
-    """
-    This function tests the second way of fitting the representation: the data is passed by storing the compounds in the
-    class.
-    """
-    test_dir = os.path.dirname(os.path.realpath(__file__))
-
-    data = np.load(test_dir + "/data/local_acsf_light.npz")
-    representation = data["arr_0"]
-    dg_dr = data["arr_1"]
-    energies = data["arr_2"]
-    forces = data["arr_3"]
-    classes = data["arr_4"]
-
-    estimator = ARMP_G()
-    estimator._set_representation(g=representation)
-    estimator.set_classes(classes=classes)
-    estimator.set_properties(energies)
+    estimator = ARMP_G(representation_name="acsf", iterations=2)
+    estimator.generate_compounds(filenames[:5])
+    estimator.set_properties(energies[:5])
     estimator.set_gradients(forces)
 
-    idx = np.arange(0, 2)
+    idx = list(range(5))
     estimator.fit(idx)
 
-def test_fit_3():
-    """
-    This function tests the thrid way of fitting the representation: the data is passed directly to the fit function.
-    """
-    test_dir = os.path.dirname(os.path.realpath(__file__))
-
-    data = np.load(test_dir + "/data/local_acsf_light.npz")
-    representation = data["arr_0"]
-    dg_dr = data["arr_1"]
-    energies = data["arr_2"]
-    forces = data["arr_3"]
-    classes = data["arr_4"]
-
-    estimator = ARMP_G()
-    estimator.fit(x=representation, y=energies, classes=classes, dy=forces, dgdr=dg_dr)
-
-def test_score_3():
+def test_score():
     """
     This function tests that all the scoring functions work.
     """
     test_dir = os.path.dirname(os.path.realpath(__file__))
 
-    data = np.load(test_dir + "/data/local_acsf_light.npz")
-    representation = data["arr_0"]
-    dg_dr = data["arr_1"]
-    energies = data["arr_2"]
-    forces = data["arr_3"]
-    classes = data["arr_4"]
+    filenames = glob.glob(test_dir + "/CN_isopentane/*.xyz")
+    energies = np.loadtxt(test_dir + '/CN_isopentane/prop_kjmol_training.txt',
+                          usecols=[1])
+    data = np.load(test_dir + "/data/CN_isopentane_forces.npz")
+    filenames.sort()
+    forces =  data["arr_3"][:5]
+    idx = list(range(5))
 
-    estimator_1 = ARMP_G(scoring_function='mae')
-    estimator_1.fit(x=representation, y=energies, classes=classes, dy=forces, dgdr=dg_dr)
-    estimator_1.score(x=representation, y=energies, classes=classes, dy=forces, dgdr=dg_dr)
+    estimator_1 = ARMP_G(scoring_function='mae', iterations=2)
+    estimator_1.generate_compounds(filenames[:5])
+    estimator_1.set_properties(energies[:5])
+    estimator_1.set_gradients(forces)
+    estimator_1.fit(idx)
+    estimator_1.score(idx)
 
-    estimator_2 = ARMP_G(scoring_function='r2')
-    estimator_2.fit(x=representation, y=energies, classes=classes, dy=forces, dgdr=dg_dr)
-    estimator_2.score(x=representation, y=energies, classes=classes, dy=forces, dgdr=dg_dr)
+    estimator_2 = ARMP_G(scoring_function='r2', iterations=2)
+    estimator_2.generate_compounds(filenames[:5])
+    estimator_2.set_properties(energies[:5])
+    estimator_2.set_gradients(forces)
+    estimator_2.fit(idx)
+    estimator_2.score(idx)
 
-    estimator_3 = ARMP_G(scoring_function='rmse')
-    estimator_3.fit(x=representation, y=energies, classes=classes, dy=forces, dgdr=dg_dr)
-    estimator_3.score(x=representation, y=energies, classes=classes, dy=forces, dgdr=dg_dr)
+    estimator_3 = ARMP_G(scoring_function='rmse', iterations=2)
+    estimator_3.generate_compounds(filenames[:5])
+    estimator_3.set_properties(energies[:5])
+    estimator_3.set_gradients(forces)
+    estimator_3.fit(idx)
+    estimator_3.score(idx)
 
-def test_predict_3():
+def test_predict():
     test_dir = os.path.dirname(os.path.realpath(__file__))
 
-    data = np.load(test_dir + "/data/local_acsf_light.npz")
-    representation = data["arr_0"]
-    dg_dr = data["arr_1"]
-    energies = data["arr_2"]
-    forces = data["arr_3"]
-    classes = data["arr_4"]
+    filenames = glob.glob(test_dir + "/CN_isopentane/*.xyz")
+    energies = np.loadtxt(test_dir + '/CN_isopentane/prop_kjmol_training.txt',
+                          usecols=[1])[:5]
+    data = np.load(test_dir + "/data/CN_isopentane_forces.npz")
+    filenames.sort()
+    filenames = filenames[:5]
+    forces =  data["arr_3"][:5]
+    idx = list(range(5))
 
-    estimator = ARMP_G()
-    estimator.fit(x=representation, y=energies, classes=classes, dy=forces, dgdr=dg_dr)
-    energies_pred, dy_pred = estimator.predict(x=representation, classes=classes, dgdr=dg_dr)
+    estimator = ARMP_G(representation_name="acsf", iterations=2)
+    estimator.generate_compounds(filenames)
+    estimator.set_properties(energies)
+    estimator.set_gradients(forces)
+    estimator.fit(idx)
+    energies_pred, dy_pred = estimator.predict(idx)
 
     assert energies.shape == energies_pred.shape
     assert forces.shape == dy_pred.shape
 
+def test_retraining():
+
+    tf.reset_default_graph()
+
+    xyz = np.array([[[0, 1, 0], [0, 1, 1], [1, 0, 1]],
+                    [[1, 2, 2], [3, 1, 2], [1, 3, 4]],
+                    [[4, 1, 2], [0.5, 5, 6], [-1, 2, 3]]])
+    zs = np.array([[1, 2, 3],
+                   [1, 2, 3],
+                   [1, 2, 3]])
+
+    ene_true = np.array([0.5, 0.9, 1.0])
+
+    forces = np.array([[[0, 1, 0], [0, 1, 1], [1, 0, 1]],
+                    [[1, 2, 2], [3, 1, 2], [1, 3, 4]],
+                    [[4, 1, 2], [0.5, 5, 6], [-1, 2, 3]]])
+
+    acsf_param = {"nRs2": 5, "nRs3": 5, "nTs": 5, "rcut": 5, "acut": 5, "zeta": 220.127, "eta": 30.8065}
+    estimator = ARMP_G(iterations=2, l1_reg=0.0001, l2_reg=0.005, learning_rate=0.0005, representation_name='acsf',
+                     representation_params=acsf_param)
+
+    estimator.set_xyz(xyz)
+    estimator.set_gradients(forces)
+    estimator.set_classes(zs)
+    estimator.set_properties(ene_true)
+
+    idx = list(range(xyz.shape[0]))
+
+    estimator.fit(idx)
+    estimator.save_nn(save_dir="temp")
+
+    pred_ene_1, pred_f_1 = estimator.predict(idx)
+
+    estimator.loaded_model = True
+
+    estimator.fit(idx)
+
+    pred_ene_2, pred_f_2 = estimator.predict(idx)
+
+    new_estimator = ARMP_G(iterations=2, l1_reg=0.0001, l2_reg=0.005, learning_rate=0.0005, representation_name='acsf',
+                         representation_params=acsf_param)
+
+    new_estimator.set_xyz(xyz)
+    new_estimator.set_classes(zs)
+    new_estimator.set_properties(ene_true)
+    new_estimator.set_gradients(forces)
+
+    new_estimator.load_nn("temp")
+
+    pred_ene_3, pred_f_3 = new_estimator.predict(idx)
+
+    new_estimator.fit(idx)
+
+    pred_ene_4, pred_f_4 = new_estimator.predict(idx)
+
+    shutil.rmtree("temp")
+
+    assert np.all(np.isclose(pred_ene_1, pred_ene_3, rtol=1.e-6))
+    assert np.all(np.isclose(pred_f_1, pred_f_3, rtol=1.e-6))
+    assert np.all(np.isclose(pred_ene_2, pred_ene_4, rtol=1.e-6))
+    assert np.all(np.isclose(pred_f_2, pred_f_4, rtol=1.e-6))
+
+
 if __name__ == "__main__":
     test_set_representation()
     test_set_properties()
-    test_set_representation()
-    test_fit_1()
-    test_fit_2()
-    test_fit_3()
-    test_score_3()
-    test_predict_3()
+    test_fit()
+    test_score()
+    test_predict()
+    test_retraining()
